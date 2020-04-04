b423c2829622e349142affc7d42dd631959c92fc
axing <axing@axings-MacBook-Pro.local>
2020-04-04 22:17:06 +0800

    init

---
+++ crawl.py
@@ -0,0 +1,153 @@
+# -*- coding: utf-8 -*-
+import base64
+from io import BytesIO
+from fontTools.ttLib import TTFont
+import requests
+import re
+import time
+import csv
+from lxml import etree
+
+
+def getRetEtree(url, params=None, **kwargs):
+    """
+        GET请求 url，并返回etree.HTML对象
+    """
+    res = requests.get(url, params=params, **kwargs)
+    return etree.HTML(res.text), res.text
+
+def houseListByHTML(html):
+    return html.xpath('.//ul[@class="house-list"]//li')
+
+def houseLocaltion(url, params=None, **kwargs):
+    res = getRetEtree(url, params=params, **kwargs)
+    local = res.xpath('.//div[@class="view-more-detailmap view-more"]/a/@href')
+    print(local, len(local))
+    if len(local) < 1:
+        return ""
+    return local[0]
+ 
+# url = 'https://km.58.com/chuzu/?PGTID=0d100000-0021-d7e2-ad8d-8d1c095b8eec&ClickID=6'
+headers = {
+    'accept':'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+    'accept-encoding':'gzip, deflate, br',
+    'accept-language':'en-US,en;q=0.9,zh-CN;q=0.8,zh;q=0.7',
+    'cache-control':'max-age=0',
+    'cookie':'f=n; commontopbar_new_city_info=541%7C%E6%98%86%E6%98%8E%7Ckm; commontopbar_ipcity=xuanhan%7C%E5%AE%A3%E6%B1%89%7C0; userid360_xml=C5CCCC32810763013C6516E934D472A1; time_create=1588491018977; myLat=""; myLon=""; id58=h6MO6l6GkxIPvDlJN+cUrg==; mcity=xuanhan; 58tj_uuid=1755d697-493e-4178-8f15-654f69a692dc; als=0; wmda_uuid=209e578c0ebf2a3b862dc046acf8d422; wmda_new_uuid=1; wmda_visited_projects=%3B11187958619315; xxzl_deviceid=KLp7hrXpSv55HfRFu0vZ6lY9Dmr18hzFWJibDfGn%2B4oR%2FvZtU2am9Av2wABcC3Aq; Hm_lvt_ae019ebe194212c4486d09f377276a77=1585899009; Hm_lpvt_ae019ebe194212c4486d09f377276a77=1585899009; f=n; commontopbar_new_city_info=541%7C%E6%98%86%E6%98%8E%7Ckm; city=km; 58home=km; commontopbar_ipcity=xuanhan%7C%E5%AE%A3%E6%B1%89%7C0; Hm_lvt_dcee4f66df28844222ef0479976aabf1=1585908296; Hm_lpvt_dcee4f66df28844222ef0479976aabf1=1585908296; new_uv=4; utm_source=; spm=; wmda_session_id_11187958619315=1585910397642-081273dd-42f7-cb8c; new_session=0; xzfzqtoken=5e%2ByBHvDgFDmc6mhB4%2BQFszXJ8hq8KJUV8G4n1Uc8f%2BJeFHnGV57EC%2FbvY2fb6S1in35brBb%2F%2FeSODvMgkQULA%3D%3D; init_refer=https%253A%252F%252Fwww.shiyanlou.com%252Fcourses%252F599%252Flearning%252F; ppStore_fingerprint=C6952153A9FD9444D324EA48597F8065286DA113ACE99431%EF%BC%BF1585911319389; xxzl_cid=72f9ec92442a46889487f11b9eb74446; xzuid=3df1d8ca-9376-4add-9e8d-fef44b916012',
+    'referer':'https://km.58.com/',
+    'sec-fetch-dest':'document',
+    'sec-fetch-mode':'navigate',
+    'sec-fetch-site':'same-origin',
+    'sec-fetch-user':'?1',
+    'upgrade-insecure-requests':'1',
+    'user-agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.149 Safari/537.36'
+}
+
+def codeb64Str(s):
+    def inner():
+        return s
+    return inner
+
+def empty():
+    return ""
+
+codeb64StrFunc=empty
+
+def decode(string):
+    font = TTFont(BytesIO(base64.decodebytes(codeb64StrFunc().encode())))
+    c = font.getBestCmap()
+    ret_list = []
+    for char in string:
+        decode_num = ord(char)
+        if decode_num in c:
+            num = c[decode_num]
+            num = int(num[-2:])-1
+            ret_list.append(num)
+        else:
+            ret_list.append(char)
+    ret_str_show = ''
+    for num in ret_list:
+        ret_str_show += str(num)
+    return ret_str_show
+
+
+def matchOrEmpty(onehouse, cond):
+    ret = onehouse.xpath(cond)
+    if (len(ret) < 1):
+        return ""
+    return ret[0]
+
+def houseInfo(hlist):
+    ret = []
+    for one in hlist:
+        title = matchOrEmpty(one,'.//div[@class="des"]/h2/a/text()').strip()
+        title = decode(title)
+        price = matchOrEmpty(one,'.//div[@class="money"]/b/text()')
+        price = decode(price)
+        adress = matchOrEmpty(one,'.//div[@class="des"]/p/a/text()')
+        adress = decode(adress)
+        houseInfoURL = matchOrEmpty(one,'.//div[@class="des"]/h2/a/@href')
+        # print(houseInfoURL)
+        # print(title)
+        # print(price)
+        # print(adress)
+        # [房源名称，地址，月租，房源url地址]
+        content = [title, adress, price, houseInfoURL]
+        # 判断是否为空
+        flag = False
+        for i in range(len(content)):
+            if content[i] == 0:
+                flag = True
+                break
+        if flag:
+            continue
+        time.sleep(2)
+        # s = ",".join(content)
+        ret.append(content)
+    return ret
+
+
+def writeCSV(hlist):
+    csv_file = open("kunming.csv","a+")
+    csv_writer = csv.writer(csv_file, delimiter=",")
+    for i in range(len(hlist)):
+        csv_writer.writerow(hlist[i])
+    csv_file.close()
+    return
+
+# 第一页 https://km.58.com/chuzu/
+# 第二页 https://km.58.com/chuzu/pn2
+# 第三页 https://km.58.com/chuzu/pn3
+kunming58URL='https://km.58.com/chuzu/'
+def page(pageURL):
+    html, text = getRetEtree(pageURL, headers=headers)
+    bstr = re.findall("charset=utf-8;base64,(.*?)'\)", text)
+    if (len(bstr) <1):
+        return
+    global codeb64StrFunc
+    codeb64StrFunc = codeb64Str(bstr[0])
+    hlist = houseListByHTML(html)
+    content = houseInfo(hlist)
+    if (len(hlist) < 1):
+        print(pageURL,"获取失败")
+        return
+    # 写入csv
+    writeCSV(content)
+
+def pageInit():
+    #计算页数
+    print("crawl", kunming58URL, "."*6)
+    page(kunming58URL)
+    for i in range(69):
+        time.sleep(5)
+        url = kunming58URL+"{0}{1}".format("pn", i+1)
+        print("crawl", url,"."*6)
+        page(url)
+    # pass
+
+if __name__ == "__main__":
+    pageInit()
+
+""" csv
+[房源名称，地址，月租，房源url地址]。
+"""
---
+++ index.js
@@ -0,0 +1,256 @@
+var map = new AMap.Map("container", {
+    resizeEnable: true,
+    zoomEnable: true,
+    // 11448159,25.012759
+    center: [102.715915, 25.012759],
+    zoom: 13
+});
+
+// 添加标尺，参考带功能控件的地图。
+var scale = new AMap.Scale();
+map.addControl(scale);
+
+//公交到达圈对象
+var arrivalRange = new AMap.ArrivalRange();
+//步行
+// var walking = new AMap.Walking();
+//经度，纬度，时间（用不到），通勤方式（默认是地铁＋公交）
+var x, y, t, vehicle = "SUBWAY,BUS";
+//工作地点，工作标记
+var workAddress, workMarker;
+//房源标记队列
+var rentMarkerArray = [];
+//多边形队列，存储公交到达的计算结果
+var polygonArray = [];
+//路径规划
+var amapTransfer;
+
+//信息窗体对象,在房源标记被点击时弹出，参考给多个点添加信息窗体。
+var infoWindow = new AMap.InfoWindow({
+    offset: new AMap.Pixel(0, -30)
+});
+
+// // //地址补完的使用，参考输入提示后查询。
+// var auto = new AMap.Autocomplete({
+//     //通过id指定输入元素
+//     input: "work-location"
+// });
+
+// //添加事件监听，在选择补完的地址后调用workLocationSelected
+// AMap.event.addListener(auto, "select", workLocationSelected);
+
+function takeBus(radio) {
+    vehicle = radio.value;
+    loadWorkLocation()
+}
+
+function takeSubway(radio) {
+    vehicle = radio.value;
+    loadWorkLocation()
+}
+
+function walking(radio) {
+    vehicle = radio.value;
+    loadWorkLocation()
+}
+
+function importRentInfo(fileInfo) {
+    // console.log(fileInfo.files[0].name)
+    // return
+    var file = fileInfo.files[0].name;
+    console.log(file)
+    loadRentLocationByFile(file);
+}
+
+//更新工作地点，加载公交到达圈
+function workLocationSelected(e) {
+    workAddress = e.poi.name;
+    // console.log(e)
+    loadWorkLocation();
+}
+
+setInterval(() => {
+    showWorklocation()
+}, 1000);
+
+function loadWorkMarker(x, y, locationName) {
+    workMarker = new AMap.Marker({
+        map: map,
+        title: locationName,
+        icon: 'http://webapi.amap.com/theme/v1.3/markers/n/mark_r.png',
+        position: [x, y]
+    });
+}
+
+// function walkingRange(x,y,t,color,v) {
+//     walking.search([x,y], t, function(status, result) {
+        
+//     })
+// }
+
+// 在地图上绘制到达圈，参考：公交到达圈。
+function loadWorkRange(x, y, t, color, v) {
+    arrivalRange.search([x, y], t, function(status, result) {
+        if (result.bounds) {
+            for (var i = 0; i < result.bounds.length; i++) {
+                var polygon = new AMap.Polygon({
+                    map: map,
+                    fillColor: color,
+                    fillOpacity: "0.4",
+                    strokeColor: color,
+                    strokeOpacity: "0.8",
+                    strokeWeight: 1
+                });
+                polygon.setPath(result.bounds[i]);
+                polygonArray.push(polygon);
+            }
+        }
+    }, {
+        policy: v
+    });
+}
+
+function addMarkerByAddress(obj) {
+    address = obj.address
+    var geocoder = new AMap.Geocoder({
+        city: obj.city,
+        radius: 1000
+    });
+    geocoder.getLocation(address, (status, result)=> {
+        if (status === "complete" && result.info === 'OK') {
+            var geocode = result.geocodes[0];
+            rentMarker = new AMap.Marker({
+                map: map,
+                title: obj.title,
+                icon: 'http://webapi.amap.com/theme/v1.3/markers/n/mark_b.png',
+                position: [geocode.location.getLng(), geocode.location.getLat()]
+            });
+            rentMarkerArray.push(rentMarker);
+            rentMarker.content = "<div>房源：<a target = '_blank' href='" + obj.url + "'>" + obj.title + "</a>"+
+            "<p>房价："+obj.price+"</p>"+
+            "<p>来源："+obj.source+"</p>"+"<div>"
+            rentMarker.address = obj.address
+            // 在房源标记被点击时打开
+            rentMarker.on('click', (e)=> {
+                workAddress = document.getElementById('work-location').value
+                //鼠标移到标记上会显示标记content属性的内容
+                infoWindow.setContent(e.target.content);
+                //在标记的位置打开窗体
+                infoWindow.open(map, e.target.getPosition());
+                if( !workAddress && workAddress == "") {
+                    console.log('没有选择工作地，所以没有路径规划')
+                    return
+                }
+                if (amapTransfer) amapTransfer.clear();
+                amapTransfer = new AMap.Transfer({
+                    map: map,
+                    policy: AMap.TransferPolicy.LEAST_TIME,
+                    city: obj.city,
+                    panel: 'transfer-panel'
+                });
+                amapTransfer.search([{
+                    keyword: e.target.address
+                }, {
+                    keyword: workAddress
+                }], function(status, result) {})
+            });
+        }
+    })
+}
+
+function delWorkLocation() {
+    if (polygonArray) map.remove(polygonArray);
+    if (workMarker) map.remove(workMarker);
+    polygonArray = [];
+}
+
+function delRentLocation() {
+    if (rentMarkerArray) map.remove(rentMarkerArray);
+    rentMarkerArray = [];
+}
+
+function showWorklocation() {
+    if (workMarker) map.remove(workMarker);
+    var flag = false
+    if (workAddress != document.getElementById('work-location').value) {
+        if (polygonArray) {map.remove(polygonArray);flag=true}
+    }
+    workAddress = document.getElementById('work-location').value
+    var geocoder = new AMap.Geocoder({
+        city: "昆明市",
+        radius: 1000
+    });
+    geocoder.getLocation(workAddress, function(status, result) {
+        if (status === "complete" && result.info === 'OK') {
+            var geocode = result.geocodes[0];
+            x = geocode.location.getLng();
+            y = geocode.location.getLat();
+            //加载工作地点标记
+            loadWorkMarker(x, y);
+            if (flag) {
+                //加载60分钟内工作地点到达圈
+                loadWorkRange(x, y, 60, "#3f67a5", vehicle);
+            }
+            //地图移动到工作地点的位置
+            // map.setZoomAndCenter(12, [x, y]);
+        }
+    })
+}
+
+function loadWorkLocation() {
+    //首先清空地图上已有的到达圈
+    delWorkLocation();
+    var geocoder = new AMap.Geocoder({
+        city: "昆明市",
+        radius: 1000
+    });
+
+    geocoder.getLocation(workAddress, function(status, result) {
+        if (status === "complete" && result.info === 'OK') {
+            var geocode = result.geocodes[0];
+            x = geocode.location.getLng();
+            y = geocode.location.getLat();
+            //加载工作地点标记
+            loadWorkMarker(x, y);
+            //加载60分钟内工作地点到达圈
+            loadWorkRange(x, y, 60, "#3f67a5", vehicle);
+            //地图移动到工作地点的位置
+            map.setZoomAndCenter(12, [x, y]);
+        }
+    })
+}
+
+function loadRentLocationByFile(fileName) {
+    //先删除现有的房源标记
+    delRentLocation();
+    //所有的地点都记录在集合中
+    var rent_locations = new Set();
+    //jquery操作
+    $.get(fileName, function(data) {
+        data = data.split("\n");
+        data.forEach(function(item, index) {
+            items = item.split(",")
+            // [房源名称，地址，月租，房源url地址]。
+            if (!items || items.length < 1 || items[0].length < 0) {
+                return
+            }
+            obj={
+                city:"昆明市",
+                source: "58同城",
+                title:items[0],
+                address:items[1],
+                price:items[2],
+                url:items[3]
+            }
+            rent_locations.add(obj);
+        });
+        rent_locations.forEach(function(element, index) {
+            //加上房源标记
+            addMarkerByAddress(element);
+        });
+    });
+}
+
+function wwww(params) {
+    console.log("aj ndoa")
+}
